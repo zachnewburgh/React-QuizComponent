784e7ac70b664dc739ee50a7f33eb60411906514
Zach Newburgh <zach.newburgh@blackrock.com>
2019-04-11 15:44:47 -0400

    feat(quiz): reset game with link

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,6 +18,10 @@ class Quiz extends Component {
         this.setState({quiz_position: this.state.quiz_position + 1})
     }
 
+    handleResetClick() {
+        this.setState({quiz_position: 1});
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
@@ -25,7 +29,7 @@ class Quiz extends Component {
             <div>
                 { 
                     isQuizEnd ? 
-                    <QuizEnd /> : 
+                    <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : 
                     <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
                 }
             </div>
