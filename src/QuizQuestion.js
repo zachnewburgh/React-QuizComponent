21a2e2fc6b9c905c063c82b59505ec09338c61b7
Zach Newburgh <zach.newburgh@blackrock.com>
2019-04-11 15:40:25 -0400

    feat(quiz-question): show error message when the wrong answer is selected

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            incorrectAnswer: false
+        }
+    }
+
     handleClick(buttonText) {
-        if (buttonText === this.props.quiz_question.answer) {
+        const isCorrectAnswer = buttonText === this.props.quiz_question.answer;
+        this.setState({incorrectAnswer: !isCorrectAnswer});
+        if (isCorrectAnswer) {
             this.props.showNextQuestionHandler();
         }
     }
@@ -23,6 +34,7 @@ class QuizQuestion extends Component {
                         }
                     </ul>
                 </section>
+                { this.state.incorrectAnswer && <p className="error">Sorry, that's not right</p> }
             </main>
         )
     }
