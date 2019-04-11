05e9822635d8e6f8aaabb984e6b557134b61958a
Zach Newburgh <zach.newburgh@blackrock.com>
2019-04-11 14:33:50 -0400

    feat(quiz): check if selected answer is correct and determine whether to provide next question

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,12 +14,20 @@ class Quiz extends Component {
         }
     }
 
+    showNextQuestion() {
+        this.setState({quiz_position: this.state.quiz_position + 1})
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
         return (
             <div>
-                { isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
+                { 
+                    isQuizEnd ? 
+                    <QuizEnd /> : 
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
+                }
             </div>
         )
     }
